c5949a2ba3c4250b53e1546a1bc9ac3f0a0d7683
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-09-08 23:39:51 -0300

    Configurando a navegacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './Routes';
 
 const App: React.FC = () => (
-  <>
+  // Sempre preciso utilizar esse navigationContainer por volta das rotas
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ backgroundColor: '#312e38', flex: 1 }} />
-  </>
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 
 export default App;
