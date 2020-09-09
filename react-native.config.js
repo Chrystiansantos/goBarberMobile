a2b1e97c687f15ebf647bf76e74a4bf365e05098
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-09-09 00:14:41 -0300

    importando fonts externas

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {}
+  },
+  assets: [
+    // irei copiar os assets da minha pasta para os arquivos nativos
+    './assets/fonts'
+  ]
+};
